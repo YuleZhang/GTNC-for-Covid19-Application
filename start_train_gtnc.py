e0ee26bea49e22e9211ea915de11baefe9023a2f
yulezhang <2592879772@qq.com>
2020-12-19 16:46:24 +0800

    add parameters saved function and some experiments result

--- start_train_gtnc.py
+++ start_train_gtnc.py
@@ -3,12 +3,15 @@ from library import MPSMLclass
 from library import Parameters
 import matplotlib.pyplot as plt
 import numpy as np
+import json
+import copy
 
 para = Parameters.gtnc()
 # get the relation between cutting dimention and testing accuracy
 batch_bond = []
 batch_acc = []
-for i in range(5, 6):
+saved_para = dict()
+for i in range(1, 9):
     bond = 2 ** i # get the cutting bond
     para['virtual_bond_limitation'] = bond
     print('The current cutting bond is %d' % bond)
@@ -17,12 +20,23 @@ for i in range(5, 6):
     acc = A.calculate_accuracy('test')
     print('Bond {0} acc is {1}'.format(bond, acc))
     batch_bond.append(bond)
+    # para[''] = acc
     batch_acc.append(acc)
+    counterpart_para = copy.deepcopy(para)
+    for item in counterpart_para.keys():
+        counterpart_para[item] = str(counterpart_para[item])
+    counterpart_para['Accuracy'] = acc
+    saved_para["bond"+str(bond)] = counterpart_para
+
+f2 = open('new_json.json', 'a+')
+json.dump(saved_para, f2)
+f2.close()
 bond_index = np.arange(len(batch_bond))
 plt.plot(bond_index, batch_acc,color='k',linestyle='-',marker = 'o',markerfacecolor='r',markersize = 10)
 plt.plot(bond_index, [0.95 for i in bond_index])
 plt.xticks(bond_index,batch_bond)
 plt.xlabel('χ(GTNC)')
 plt.ylabel('test accuracy')
+plt.savefig('test.jpg')
 plt.show()
 
